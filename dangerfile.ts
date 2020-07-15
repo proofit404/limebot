bd9c8f78e9a4faa1a8d8ae0ede4a74c3c9a6294e
Artem Malyshev <proofit404@gmail.com>
2020-07-15 11:19:43 +0300

    Document DangerJS installation (#21).

---
+++ dangerfile.ts
@@ -0,0 +1,16 @@
+// @todo #21 Pull request title should start with capital letter.
+// @todo #21 Issue title should start with capital letter.
+// @todo #21 Commit message should start with capital letter.
+
+// @todo #21 Pull request title should ends with the dot.
+// @todo #21 Issue title should ends with the dot.
+// @todo #21 Commit message should ends with the dot.
+
+// @todo #21 Commit title SHOULD contain issue number.
+// @todo #21 Pull request title SHOULD NOT contain issue number.
+// @todo #21 Pull request body SHOULD contain issue number.
+
+// @todo #21 Multiline commit message should separate title with new line.
+
+// @todo #21 Pull request should not have labels.
+// @todo #21 Issue should not have labels.
