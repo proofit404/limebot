eb6548ba40ba2c900de345f5ebd2765d7fe5bff7
Artem Malyshev <proofit404@gmail.com>
2020-08-17 15:09:07 +0300

    Convert standard xml tree to the lxml tree (#114).

--- src/app/web/pages/index.py
+++ src/app/web/pages/index.py
@@ -1,6 +1,4 @@
-from io import StringIO
-
-from lxml.etree import parse
+from lxml.etree import fromstring
 from lxml.etree import tostring
 from lxml.etree import XML
 from lxml.etree import XSLT
@@ -9,15 +7,11 @@ from xmlschema import XMLSchema
 
 
 class Index:
-    def __init__(self):
-        self.title = "Lime"
-        self.message = "Hello world."
-
     def render(self):
         transform = XSLT(XML(open("src/app/web/xsl/index.xsl").read()))
         schema = XMLSchema("src/app/web/xsd/index.xsd")
-        data = {"title": self.title, "message": self.message}
-        xml = etree_tostring(schema.encode(data))
-        # @todo #111 Avoid encoding to string and decoding back to etree.
-        html = tostring(transform(parse(StringIO(xml))))
+        data = {"title": "Lime", "message": "Hello world."}
+        xml = schema.encode(data)
+        # @todo #114 Title & message are absent in the output HTML document.
+        html = tostring(transform(fromstring(etree_tostring(xml))))
         return html
