ec8550ebea7eb8e4c4389167a4f5143689271429
Artem Malyshev <proofit404@gmail.com>
2020-08-18 11:12:25 +0300

    Test `Index.render` method (#113).

--- src/app/web/pages/index.py
+++ src/app/web/pages/index.py
@@ -1,8 +1,7 @@
-from lxml.etree import fromstring
+from lxml.etree import Element
 from lxml.etree import tostring
 from lxml.etree import XML
 from lxml.etree import XSLT
-from xmlschema import etree_tostring
 from xmlschema import XMLSchema
 
 
@@ -11,6 +10,6 @@ class Index:
         transform = XSLT(XML(open("src/app/web/xsl/index.xsl").read()))
         schema = XMLSchema("src/app/web/xsd/index.xsd")
         data = {"title": "Lime", "message": "Hello world."}
-        xml = schema.encode(data)
-        html = tostring(transform(fromstring(etree_tostring(xml))))
+        xml = schema.encode(data, etree_element_class=Element)
+        html = tostring(transform(xml))
         return html
--- tests/web/pages/test_index.py
+++ tests/web/pages/test_index.py
@@ -1,1 +1,8 @@
-# @todo #111 Test `Index.render` method.
+from app.web.pages.index import Index
+
+
+def test_index_render():
+    """The `render` method of the `Index` class should return HTML."""
+    index = Index()
+    html = index.render()
+    assert html.startswith(b"<html")
